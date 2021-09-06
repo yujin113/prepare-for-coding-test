9f91b1c8f4417d5148b9d536461e835969991975
yujin113 <claire9585@sookmyung.ac.kr>
2021-09-06 15:18:33 +0900

    11399_ATM

---
+++ Greedy/Greedy_11399.java
@@ -0,0 +1,32 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Greedy_11399 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        int time = 0;
+        for (int i = 0; i < N; i++) {
+            time += arr[i];
+            sum += time;
+        }
+
+        System.out.println(sum);
+    }
+}
