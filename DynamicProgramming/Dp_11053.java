fa17ee2c87d1f05644a80c1aff2fc5aea699a978
yujin113 <claire9585@sookmyung.ac.kr>
2021-12-20 15:12:12 +0900

    11053_가장 긴 증가하는 부분 수열

---
+++ DynamicProgramming/Dp_11053.java
@@ -0,0 +1,37 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dp_11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (dp[i] == 0)
+                dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i : dp) {
+            max = Math.max(max, i);
+        }
+        System.out.println(max);
+    }
+}
