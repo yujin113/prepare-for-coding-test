6bb36eac1fbb14d2c6173386b9141c6ec18bf05e
yujin113 <claire9585@sookmyung.ac.kr>
2022-03-23 17:14:00 +0900

    10844_쉬운 계단 수

---
+++ DynamicProgramming/Dp_10844.java
@@ -0,0 +1,35 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Dp_10844 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[][] dp = new long[N + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j + 1] % 1000000000;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][j - 1] % 1000000000;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+                }
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i < 10; i++) {
+            result += dp[N][i];
+        }
+        System.out.println(result % 1000000000);
+    }
+}
