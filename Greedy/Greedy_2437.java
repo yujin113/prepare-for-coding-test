38bb0e38a05f532821b342b3f7feb68a021e4ac0
yujin113 <claire9585@sookmyung.ac.kr>
2022-03-28 21:51:45 +0900

    2437_저울

---
+++ Greedy/Greedy_2437.java
@@ -0,0 +1,30 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Greedy_2437 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > max + 1) {
+                break;
+            }
+            max += arr[i];
+        }
+        System.out.println(max + 1);
+    }
+}
