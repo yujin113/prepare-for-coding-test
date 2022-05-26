f54ebfbb4fd096cf4404e767e4a89635778c3a65
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-26 13:31:33 +0900

    11055_가장 큰 증가 부분 수열

---
+++ DynamicProgramming/Dp_11055.java
@@ -0,0 +1,43 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dp_11055 {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(dp(N));
+    }
+
+    private static int dp(int N) {
+        int[] dp = new int[N];
+        dp[0] = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i : dp) {
+            max = Math.max(max, i);
+        }
+        return max;
+    }
+}
