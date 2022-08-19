09e55f25bc2fc57ad0b60021d75f2e5354fc1258
yujin113 <claire9585@sookmyung.ac.kr>
2022-08-19 16:38:04 +0900

    2294_동전 2

---
+++ DynamicProgramming/Dp_2294.java
@@ -0,0 +1,36 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dp_2294 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        int[] dp = new int[k + 1];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= k; i++) {
+            dp[i] = 100001;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
+            }
+        }
+
+        if (dp[k] == 100001) {
+            System.out.println("-1");
+        } else {
+            System.out.println(dp[k]);
+        }
+    }
+}
