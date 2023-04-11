2f9fda0fb7b5f2eeab6577a59891553e11c38921
yujin113 <claire9585@sookmyung.ac.kr>
2023-04-11 10:09:23 +0900

    15486 퇴사 2

--- DynamicProgramming/Dp_15486.java
+++ DynamicProgramming/Dp_15486.java
@@ -18,25 +18,29 @@ public class Dp_15486 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		Node[] arr = new Node[N];
-		for (int i = 0; i < N; i++) {
+		Node[] arr = new Node[N + 1];
+		for (int i = 1; i <= N; i++) {
 			String[] str = br.readLine().split(" ");
 			arr[i] = new Node(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
 		}
 
-		int[] dp = new int[N + 1];
-
+		int[] dp = new int[N + 2];
 		int max = 0;
-		for (int i = 0; i < N; i++) {
-			max = Math.max(max, dp[i]);
 
+		for (int i = 1; i <= N; i++) {
+			max = Math.max(max, dp[i]);
 			int next = i + arr[i].day;
-			if (next <= N) {
+
+			if (next <= N + 1) {
 				dp[next] = Math.max(dp[next], max + arr[i].cost);
 			}
 		}
-		max = Math.max(max, dp[N]);
 
-		System.out.println(max);
+		int result = 0;
+		for (int i : dp) {
+			result = Math.max(result, i);
+		}
+
+		System.out.println(result);
 	}
 }
